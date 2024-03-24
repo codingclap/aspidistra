110ae47b75f0e32a5c5791c5bd6e92a3fd244d36
Anuj Kumar <86232374+codingclap@users.noreply.github.com>
2024-03-24 10:39:20 +0530

    Two section is done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,22 +8,32 @@ mainSwitch.addEventListener('click', function (e) {
     if (mainSwitch.value === 'light') {
         mainSwitch.value = 'dark';
         /** Light Mode */
-        
- 
-        headingColor.setAttribute('data-txt-color','#000')
-        paraColor.setAttribute('data-txt-color','#000')
-        homeBanner.style.backgroundImage="url()";
-        ourStoryBanner.style.backgroundImage="url()";
-        
+
+        for (let i = 0; i < headingColor.length; i++) {
+            headingColor[i].setAttribute('data-txt-color', '#000');
+        }
+        for (let j = 0; j < paraColor.length; j++) {
+            paraColor[j].setAttribute('data-txt-color', '#000');
+        }
+        //headingColor.setAttribute('data-txt-color','#000');
+        //paraColor.setAttribute('data-txt-color','#000');
+        homeBanner.style.backgroundImage = "url()";
+        ourStoryBanner.style.backgroundImage = "url()";
+
         console.log('light');
 
     } else if (mainSwitch.value === 'dark') {
         mainSwitch.value = 'light';
         /** Dark Mode */
-        headingColor.setAttribute('data-txt-color','green')
-        paraColor.setAttribute('data-txt-color','light')
-        homeBanner.style.backgroundImage="url(assets/img/bg-1.jpg)";
-        ourStoryBanner.style.backgroundImage="url(assets/img/our-story-bg.png)";
+        for (let i = 0; i < headingColor.length; i++) {
+            headingColor[i].setAttribute('data-txt-color', 'green'); 
+        }
+        for (let j = 0; j < paraColor.length; j++) {
+            paraColor[j].setAttribute('data-txt-color', 'light');
+        }
+
+        homeBanner.style.backgroundImage = "url(assets/img/bg-1.jpg)";
+        ourStoryBanner.style.backgroundImage = "url(assets/img/our-story-bg.png)";
 
 
         console.log('dark');
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./assets/css/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
