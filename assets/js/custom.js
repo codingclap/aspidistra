61153c56af81b58083ac0d8ff1eae0420757141e
Anuj Kumar <codingclap@gmail.com>
2024-03-27 13:15:19 +0530

    final done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,11 @@
 let mainSwitch = document.querySelector('#switch');
 let headingColor = document.querySelectorAll('.heading-dark');
 let paraColor = document.querySelectorAll('.para-dark');
+let heading_border_after = document.querySelectorAll('.border-switch');
 let homeBanner = document.querySelector('.home-banner');
 let ourStoryBanner = document.querySelector('.ourStory-banner');
+let ourValuesBanner = document.querySelector('.ourValues-banner');
+
 
 mainSwitch.addEventListener('click', function (e) {
     if (mainSwitch.value === 'light') {
@@ -19,14 +22,23 @@ mainSwitch.addEventListener('click', function (e) {
         //paraColor.setAttribute('data-txt-color','#000');
         homeBanner.style.backgroundImage = "url()";
         ourStoryBanner.style.backgroundImage = "url()";
+        ourValuesBanner.style.backgroundImage = "url()";
 
-        console.log('light');
+        for (let k = 0; k < heading_border_after.length; k++) {
+            if (heading_border_after[k].classList.contains("b-before-after")) {
+                heading_border_after[k].classList.remove("b-before-after-dark");
+                heading_border_after[k].classList.add("b-before-after-dark");
+            }
+        }
+
+
+        //console.log('light');
 
     } else if (mainSwitch.value === 'dark') {
         mainSwitch.value = 'light';
         /** Dark Mode */
         for (let i = 0; i < headingColor.length; i++) {
-            headingColor[i].setAttribute('data-txt-color', 'green'); 
+            headingColor[i].setAttribute('data-txt-color', 'green');
         }
         for (let j = 0; j < paraColor.length; j++) {
             paraColor[j].setAttribute('data-txt-color', 'light');
@@ -34,8 +46,17 @@ mainSwitch.addEventListener('click', function (e) {
 
         homeBanner.style.backgroundImage = "url(assets/img/bg-1.jpg)";
         ourStoryBanner.style.backgroundImage = "url(assets/img/our-story-bg.png)";
+        ourValuesBanner.style.backgroundImage = "url(assets/img/our-story-bg.png)";
 
+        for (let k = 0; k < heading_border_after.length; k++) {
+            if (heading_border_after[k].classList.contains("b-before-after-dark")) {
+                heading_border_after[k].classList.remove("b-before-after-dark");
+                heading_border_after[k].classList.add("b-before-after");
+            }
+        }
 
-        console.log('dark');
+        //console.log('dark');
     }
+
+
 });
