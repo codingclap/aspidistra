a3ede5513f915fb811f4868f11e3d029bf26bdfc
Anuj Kumar <codingclap@gmail.com>
2024-03-29 16:52:49 +0530

    dark and light mode done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,65 +1,74 @@
 let mainSwitch = document.querySelector('#switch');
 let headingColor = document.querySelectorAll('.heading-dark');
 let paraColor = document.querySelectorAll('.para-dark');
+let paraOneColor = document.querySelectorAll('.para1-dark');
 let heading_border_after = document.querySelectorAll('.border-switch');
 let homeBanner = document.querySelector('.home-banner');
 let ourStoryBanner = document.querySelector('.ourStory-banner');
 let ourValuesBanner = document.querySelector('.ourValues-banner');
 let navbarPanel = document.querySelector('.navbar');
+let ourStory = document.querySelector('.ourStory');
+let footerImg = document.querySelector('.footer-img');
 
 mainSwitch.addEventListener('click', function (e) {
-    if (mainSwitch.value === 'light') {
-        mainSwitch.value = 'dark';
-        /** Light Mode */
+  if (mainSwitch.value === 'light') {
+    mainSwitch.value = 'dark';
+    /** Light Mode */
 
-        for (let i = 0; i < headingColor.length; i++) {
-            headingColor[i].setAttribute('data-txt-color', '#000');
-        }
-        for (let j = 0; j < paraColor.length; j++) {
-            paraColor[j].setAttribute('data-txt-color', '#000');
-        }
-        
-        //headingColor.setAttribute('data-txt-color','#000');
-        //paraColor.setAttribute('data-txt-color','#000');
-        homeBanner.style.backgroundImage = "url()";
-        navbarPanel.setAttribute('data-txt-color','black');
-        //ourStoryBanner.style.backgroundImage = "url()";
-        //ourValuesBanner.style.backgroundImage = "url()";
-
-        for (let k = 0; k < heading_border_after.length; k++) {
-            if (heading_border_after[k].classList.contains("b-before-after")) {
-                heading_border_after[k].classList.remove("b-before-after-dark");
-                heading_border_after[k].classList.add("b-before-after-dark");
-            }
-        }
-
-
-        //console.log('light');
-
-    } else if (mainSwitch.value === 'dark') {
-        mainSwitch.value = 'light';
-        /** Dark Mode */
-        for (let i = 0; i < headingColor.length; i++) {
-            headingColor[i].setAttribute('data-txt-color', 'green');
-        }
-        for (let j = 0; j < paraColor.length; j++) {
-            paraColor[j].setAttribute('data-txt-color', 'light');
-        }
-
-        navbarPanel.setAttribute('data-txt-color','black-opacity');
-        homeBanner.style.backgroundImage = "url(assets/img/bg-1.jpg)";
-       // ourStoryBanner.style.backgroundImage = "url(assets/img/our-story-bg.png)";
-       // ourValuesBanner.style.backgroundImage = "url(assets/img/our-story-bg.png)";
-
-        for (let k = 0; k < heading_border_after.length; k++) {
-            if (heading_border_after[k].classList.contains("b-before-after-dark")) {
-                heading_border_after[k].classList.remove("b-before-after-dark");
-                heading_border_after[k].classList.add("b-before-after");
-            }
-        }
-
-        //console.log('dark');
+    for (let i = 0; i < headingColor.length; i++) {
+      headingColor[i].setAttribute('data-txt-color', 'dark');
     }
+    for (let j = 0; j < paraColor.length; j++) {
+      paraColor[j].setAttribute('data-txt-color', 'dark');
+    }
+    for (let k = 0; k < paraOneColor.length; k++) {
+      paraOneColor[k].setAttribute('data-txt-color', 'dark');
+    }
+    ourStory.setAttribute('data-bg-color', 'gray-light');
+    homeBanner.style.backgroundImage = "url()";
+    navbarPanel.setAttribute('data-bg-color', 'black');
+    footerImg.src = "assets/img/aspidistra-big-dark.png";
+    //ourStoryBanner.style.backgroundImage = "url()";
+    //ourValuesBanner.style.backgroundImage = "url()";
+
+    for (let k = 0; k < heading_border_after.length; k++) {
+      if (heading_border_after[k].classList.contains("b-before-after")) {
+        heading_border_after[k].classList.remove("b-before-after-dark");
+        heading_border_after[k].classList.add("b-before-after-dark");
+      }
+    }
+
+    //console.log('light');
+
+  } else if (mainSwitch.value === 'dark') {
+    mainSwitch.value = 'light';
+    /** Dark Mode */
+    for (let i = 0; i < headingColor.length; i++) {
+      headingColor[i].setAttribute('data-txt-color', 'green');
+    }
+    for (let j = 0; j < paraColor.length; j++) {
+      paraColor[j].setAttribute('data-txt-color', 'light');
+    }
+    for (let k = 0; k < paraOneColor.length; k++) {
+      paraOneColor[k].setAttribute('data-txt-color', 'gray');
+    }
+
+    ourStory.setAttribute('data-bg-color', 'none');
+    navbarPanel.setAttribute('data-bg-color', 'black-opacity');
+    homeBanner.style.backgroundImage = "url(assets/img/bg-1.jpg)";
+    footerImg.src = "assets/img/aspidistra-big.png";
+    // ourStoryBanner.style.backgroundImage = "url(assets/img/our-story-bg.png)";
+    // ourValuesBanner.style.backgroundImage = "url(assets/img/our-story-bg.png)";
+
+    for (let k = 0; k < heading_border_after.length; k++) {
+      if (heading_border_after[k].classList.contains("b-before-after-dark")) {
+        heading_border_after[k].classList.remove("b-before-after-dark");
+        heading_border_after[k].classList.add("b-before-after");
+      }
+    }
+
+    //console.log('dark');
+  }
 
 
 });
@@ -72,27 +81,27 @@ mainSwitch.addEventListener('click', function (e) {
 /********** Mobile Navbars  ********/
 
 let navButton = document.querySelector('.dropdown-navbar');
-    let firstBar = document.querySelector('.navbar-toggler-icon-1:nth-of-type(1)');
-    let middleBar = document.querySelector('.navbar-toggler-icon-1:nth-of-type(2)');
-    let lastBar = document.querySelector('.navbar-toggler-icon-1:nth-of-type(3)');
-    navButton.addEventListener('click', () => {
-      if (navButton.classList.contains('inactive')) {
-        firstBar.style = "transform:rotate(145deg)";
-        middleBar.style = "display:none;";
-        lastBar.style = "transform:rotate(220deg);margin-top:-2px";
+let firstBar = document.querySelector('.navbar-toggler-icon-1:nth-of-type(1)');
+let middleBar = document.querySelector('.navbar-toggler-icon-1:nth-of-type(2)');
+let lastBar = document.querySelector('.navbar-toggler-icon-1:nth-of-type(3)');
+navButton.addEventListener('click', () => {
+  if (navButton.classList.contains('inactive')) {
+    firstBar.style = "transform:rotate(145deg)";
+    middleBar.style = "display:none;";
+    lastBar.style = "transform:rotate(220deg);margin-top:-2px";
 
-        navButton.classList.remove('inactive');
-      } else {
-        navButton.classList.add('inactive');
-        firstBar.style = "transform:rotate(0deg)";
-        middleBar.style = "display:block;";
-        lastBar.style = "transform:rotate(0deg);margin-top:6px";
-      }
+    navButton.classList.remove('inactive');
+  } else {
+    navButton.classList.add('inactive');
+    firstBar.style = "transform:rotate(0deg)";
+    middleBar.style = "display:block;";
+    lastBar.style = "transform:rotate(0deg);margin-top:6px";
+  }
 
 
-      //console.log(middleBar);
+  //console.log(middleBar);
 
-    })
+})
 
 /********** Mobile Navbars  ********/
 
@@ -103,7 +112,7 @@ let navButton = document.querySelector('.dropdown-navbar');
 /**************** Services Hide and Show **************/
 
 let serviceReadBtn = document.querySelectorAll('a.read-more');
-let items_id = [];                                                                      
+let items_id = [];
 for (let i = 0; i < serviceReadBtn.length; i++) {
 
   let box_className = serviceReadBtn[i].classList[2];
